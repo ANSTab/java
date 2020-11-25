5d1b0bf6507c098911b019c1ad7a25f664c76898
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-11-25 08:54:35 +0200

    виловив виключення

--- scr/com/tabachenko/task5/FileMeneger.java
+++ scr/com/tabachenko/task5/FileMeneger.java
@@ -1,9 +1,9 @@
 package com.tabachenko.task5;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-
 
 public class FileMeneger {
 
@@ -14,39 +14,36 @@ public class FileMeneger {
         File dir1 = new File("D:\\TestDir\\pictures");
         File dir2 = new File("D:\\TestDir\\text");
 
-
         if (!dir.exists() && !dir1.exists() && !dir2.exists()) {
             dir.mkdir();
             dir1.mkdir();
             dir2.mkdir();
-            System.out.println("YES");
+            System.out.println("Сортування проведене");
         } else {
-            System.out.println("Allredy YES");
+            System.out.println("вже є такі дерикторії");
         }
-
-        for (File f : dir0.listFiles()) {
-            if (f.getName().toLowerCase().endsWith(".mp3")) {
-                File qFile = new File("D:\\TestDir\\musikNew\\" + f.getName());
-                Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                f.delete();
-
+        try {
+            for (File f : dir0.listFiles()) {
+                if (f.getName().toLowerCase().endsWith(".mp3")) {
+                    File qFile = new File("D:\\TestDir\\musikNew\\" + f.getName());
+                    Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    f.delete();
+                }
+                if (f.getName().toLowerCase().endsWith(".txt")) {
+                    File qFile = new File("D:\\TestDir\\text\\" + f.getName());
+                    Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    f.delete();
+                }
+                if (f.getName().toLowerCase().endsWith(".jpg")) {
+                    File qFile = new File("D:\\TestDir\\pictures\\" + f.getName());
+                    Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    f.delete();
+                }
+                if (!f.exists()) {
+                    dir0.delete();
+                }
             }
-            if (f.getName().toLowerCase().endsWith(".txt")) {
-                File qFile = new File("D:\\TestDir\\text\\" + f.getName());
-                Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                f.delete();
-
-            }
-            if (f.getName().toLowerCase().endsWith(".jpg")) {
-                File qFile = new File("D:\\TestDir\\pictures\\"+f.getName());
-                  Files.copy(f.toPath(), qFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                  f.delete();
-
-
-            }
-            if (!f.exists()){
-                dir0.delete();
-            }
+        } catch (NullPointerException e) {
 
         }
     }
