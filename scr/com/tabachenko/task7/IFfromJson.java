61e8c60512c4a60388a397df9cfdd035775748f0
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-12-09 11:52:01 +0200

    Парсінг Json, та Розпарсінг з Json файла

--- scr/com/tabachenko/task7/IFfromJson.java
+++ scr/com/tabachenko/task7/IFfromJson.java
@@ -1,8 +1,6 @@
 package com.tabachenko.task7;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class IFfromJson {
     public boolean serializetion(String infoTask) {
@@ -36,26 +34,18 @@ public class IFfromJson {
     }
 
     public StringBuilder deserializatior() throws InvalidObjectException {
-        //  File file = new File("D:\\java\\jSonFile.json");
-        File file = new File("D:\\java\\responce.json");
-        ObjectInputStream ois = null;
+           File file = new File("D:\\java\\responce.json");
         String InfoTask = null;
         StringBuilder stringBuilder = new StringBuilder();
-        List<String> list = new ArrayList<>();
+
         try {
             FileInputStream fis = new FileInputStream(file);
             if (fis != null) {
                 int b = 0;
                 while ((b = fis.read()) != -1) {
-                    // ois = new ObjectInputStream(fis);
-                    // String InfoTask = (String) ois.readObject();
                     InfoTask = Character.toString(b);
                     stringBuilder.append(InfoTask);
-                    //System.out.println(stringBuilder);
-                    //System.out.print(InfoTask);
-                    //System.out.print((char)b);
                 }
-
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -64,15 +54,6 @@ public class IFfromJson {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
-        /*finally {
-          /*  try {
-                ois.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-        //  throw new InvalidObjectException(" Object fail");
-        //System.out.println(InfoTask);
         return stringBuilder;
     }
 }
