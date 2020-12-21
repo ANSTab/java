9e99cb111fc1a7a90d5688d8cd2d4c459dc0f2ec
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-12-21 23:45:37 +0200

    конекшек до сіквела1

---
+++ scr/com/tabachenko/task4/chornovik.java
@@ -0,0 +1,26 @@
+package com.tabachenko.task4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class chornovik {
+   /* public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("b");
+        stringList.add("a");
+        stringList.add("c");
+        stringList.add("v");
+        stringList.add("v");
+        ListIterator<String> s = stringList.listIterator();
+        for (String w: stringList){
+            while (s.hasNext()) {
+                if (w.equals(w.)) {
+                    System.out.println("є дублі");
+                }
+                System.out.println(s.next() + "");
+            }
+        }
+    }*/
+}
+
