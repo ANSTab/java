849941e003283f50796bbd18c50d2c0e6572577b
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-12-22 23:22:21 +0200

    конекшек до сіквела1

---
+++ scr/com/tabachenko/task8/test.java
@@ -0,0 +1,17 @@
+package com.tabachenko.task8;
+
+public class test {
+
+    public static void main(String[] args) {
+        //java -classpath c:\Java\mysql-connector-java-8.0.11.jar;c:\Java Program
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Connection succesfull!");
+        } catch (Exception ex) {
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+}
+
