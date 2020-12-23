f85e1693ce636bb098cc3a783074cb54689fb8ec
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-12-23 23:22:12 +0200

    конекшен з базой і перший запит

--- src/main/java/com/tabachenko/task8/TaskMysql.java
+++ src/main/java/com/tabachenko/task8/TaskMysql.java
@@ -1,15 +1,24 @@
 package com.tabachenko.task8;
 
-
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TaskMysql {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mybase", "ANSTab", "Alex17101988") ;
-        System.out.println(connection);
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mybase", "ANSTab", "Alex17101988");
+        // System.out.println("Є конекшен");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select country.name as 'назва країни',count(DISTINCT  region.name) as 'кількість регіонів'  , count(city.name) as 'кількість міст'\n" +
+                "from  country left join region  on  country.id = region.country_id  \n" +
+                " left join city on region.id = city.region_id\n" +
+                "group by country.name\n" +
+                "order by count(DISTINCT region.name) desc;");
+        System.out.printf("%25s%35s%36s","Назва країни","Кількість регіонів","Кількість міст");
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        while (resultSet.next()) {
+            System.out.printf("%25s%30s%36s", resultSet.getString(1) + " ",resultSet.getInt(2) + " ",resultSet.getInt(3) + " "+"\n");
+        }
     }
 }
