0f28702b83227884141e7e93f5051d2123cc304b
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2020-12-26 17:58:12 +0200

    Update work task8

--- src/main/java/com/tabachenko/task8/LauncherSqlBase.java
+++ src/main/java/com/tabachenko/task8/LauncherSqlBase.java
@@ -4,15 +4,25 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class TaskMysql {
+public class LauncherSqlBase {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        int idCountru = 17101988;
+        String coutnryName = "ANSTAB";
+
+        int idRegion = 22111999;
+        String regionName = "ANSTAB region.";
+        String cityName = "Sofiy";
 
         SqlTask sqlTask = new SqlTask();
+        sqlTask.DatabaseWithoutMysql();
+        //sqlTask.DatabaseUpd(idCountru, coutnryName, idRegion, regionName, cityName);
         //System.out.println(sqlTask.SqlGetListObj());
-        //sqlTask.DatabaseWithoutMysql();
-        ArrayList<CountryInfo> arrayList = sqlTask.SqlGetListObj();
+
+
+        /*ArrayList<CountryInfo> arrayList = sqlTask.SqlGetListObj();
         for (CountryInfo co : arrayList) {
             System.out.println("Країни: " + co.getCountryName() + "," + " Кількість регіонів:  " + co.getRegionCount() + "," + " Кількість міст:  " + co.getCityCount());
-        }
+        }*/
+        //sqlTask.allBase();
     }
 }
--- src/main/java/com/tabachenko/task8/SqlTask.java
+++ src/main/java/com/tabachenko/task8/SqlTask.java
@@ -3,20 +3,21 @@ package com.tabachenko.task8;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class SqlTask extends CountryInfo{
+public class SqlTask extends CountryInfo {
 
     Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mybase", "ANSTab", "Alex17101988");
     Statement statement = connection.createStatement();
 
     public SqlTask() throws SQLException {
     }
-    public ArrayList<CountryInfo> SqlGetListObj() throws SQLException {
+
+    public ArrayList SqlGetListObj() throws SQLException {
         ResultSet resultSet = statement.executeQuery("select country.name as 'назва країни',count(DISTINCT  region.name) as 'кількість регіонів'  , count(city.name) as 'кількість міст'\n" +
                 "from  country left join region  on  country.id = region.country_id  \n" +
                 " left join city on region.id = city.region_id\n" +
                 "group by country.name\n" +
                 "order by count(DISTINCT region.name) desc;");
-        ArrayList<CountryInfo> cou = new ArrayList<>();
+        ArrayList cou = new ArrayList<>();
         while (resultSet.next()) {
             CountryInfo countryInfo = new CountryInfo(resultSet.getString(1), resultSet.getInt(2), resultSet.getInt(3));
             cou.add(countryInfo);
@@ -24,18 +25,35 @@ public class SqlTask extends CountryInfo{
         }
         return cou;
     }
-    public void DatabaseWithoutMysql () throws SQLException {
-        Statement statement = connection.createStatement();
+
+    public void DatabaseWithoutMysql() throws SQLException {
         ResultSet resultSet = statement.executeQuery("select country.name as 'назва країни',count(DISTINCT  region.name) as 'кількість регіонів'  , count(city.name) as 'кількість міст'\n" +
                 "from  country left join region  on  country.id = region.country_id  \n" +
                 " left join city on region.id = city.region_id\n" +
                 "group by country.name\n" +
                 "order by count(DISTINCT region.name) desc;");
-        System.out.printf("%27s%35s%36s","Назва країни","Кількість регіонів","Кількість міст");
+        System.out.printf("%27s%35s%36s", "Назва країни", "Кількість регіонів", "Кількість міст");
         System.out.println();
         System.out.println("-------------------------------------------------------------------------------------------------");
         while (resultSet.next()) {
-            System.out.printf("%27s%30s%36s", resultSet.getString(1) + " ",resultSet.getInt(2) + " ",resultSet.getInt(3) + " "+"\n");
+            System.out.printf("%27s%30s%36s", resultSet.getString(1) + " ", resultSet.getInt(2) + " ", resultSet.getInt(3) + " " + "\n");
+        }
+    }
+
+    public void DatabaseUpd(int idCountru, String coutnryName, int idRegion, String regionName, String cityName) throws SQLException {
+        statement.executeUpdate(" insert into country(id, name) values (" + idCountru + ", '" + coutnryName + "');");
+        statement.executeUpdate("insert into region(id, country_id, name) values (" + idRegion + "," + idCountru + ", '" + regionName + "');");
+        statement.executeUpdate("insert into city(region_id, name) values (" + idRegion + ", '" + cityName + "');");
+
+        System.out.println("Database update, object created");
+    }
+
+    public void allBase() throws SQLException {
+        ResultSet resultSet = statement.executeQuery("select country.name ,region.name , city.name\n" +
+                "from  country left join region  on  country.id = region.country_id  \n" +
+                " left join city on region.id = city.region_id;");
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1)+"      "+resultSet.getString(2)+"    "+resultSet.getString(3));
         }
     }
 }
