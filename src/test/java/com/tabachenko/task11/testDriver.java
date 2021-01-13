7d5eee2c1b7901671adce3fa98bbfeb06b0e206c
Tabachenko Olexiy <ast.tovlnz@gmail.com>
2021-01-13 11:33:16 +0200

    тест на википедию

---
+++ src/test/java/com/tabachenko/task11/testDriver.java
@@ -0,0 +1,22 @@
+package com.tabachenko.task11;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class testDriver {
+    @Test
+    public void taskTest() {
+        File file = new File("D:\\Downloads\\Driver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        ChromeDriver webDriver = new ChromeDriver();
+        webDriver.get("https://ru.wikipedia.org/wiki");
+        String title = webDriver.getTitle();
+        Assert.assertTrue(title.contains("Википедия"));
+        webDriver.quit();
+    }
+}
+
+
